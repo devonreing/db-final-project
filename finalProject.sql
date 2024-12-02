a868012d5ad4bb3cee37a4bcb8c57a6c32bbc168
sgroark <156830376+sgroark@users.noreply.github.com>
2024-12-02 16:00:04 -0500

    Update finalProject.sql

--- finalProject.sql
+++ finalProject.sql
@@ -44,7 +44,8 @@ CREATE TABLE `reservations` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(255) NOT NULL,
   `party_size` int NOT NULL,
-  `reservation_time` datetime NOT NULL,
+  `reservation_time` time NOT NULL,
+  `reservation_date` date NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
