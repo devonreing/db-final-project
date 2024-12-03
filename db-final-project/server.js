bcf3fe731afd3314dc29d937569edc1caebb25d1
erinzahner <ez749184@sju.edu>
2024-12-03 09:12:06 -0500

    added styling for reservation dropdown

--- db-final-project/server.js
+++ db-final-project/server.js
@@ -10,7 +10,7 @@ const port = 3000;
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "DBFinalProject5!",
+  password: "password",
   database: "finalProject"
 });
 
